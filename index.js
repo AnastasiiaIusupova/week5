675d030172ab14c309583df2f3a185fea78a6872
AnastasiiaIusupova <47871395+AnastasiiaIusupova@users.noreply.github.com>
2020-11-29 13:47:29 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import crypto from 'crypto';
+import http from 'http';
+import bodyParser from 'body-parser';
+import { createReadStream } from 'fs';
+
+const port = 80;
+
+import initApp from './app.js';
+const app = initApp(express, bodyParser, createReadStream, crypto, http);
+
+app.listen(process.env.PORT || port);
